3ff5f8a957c224455d2875f52d9f0185dd9c74ca
harini007z <58560884+harini007z@users.noreply.github.com>
2020-06-30 19:07:11 +0700

    Update randomlink().js

--- randomlink().js
+++ randomlink().js
@@ -1,4 +1,4 @@
-var random = new Array("https://bit.ly/3dCmEIz", "https://bit.ly/3dAdw7r", "https://bit.ly/2NC5foK", "https://bit.ly/3eDyMKR", "https://bit.ly/3dFng0a" , "https://bit.ly/2A7tGav", "https://bit.ly/2YDff7N", "https://bit.ly/2BfiBEY", "https://bit.ly/2Zhzerp" );
+var random = new Array("https://detik.balitrips.site/", "https://detik.balitrips.site/", "https://detik.balitrips.site/", "https://detik.balitrips.site/", "https://detik.balitrips.site/" , "https://detik.balitrips.site/", "https://detik.balitrips.site/", "https://detik.balitrips.site/", "https://detik.balitrips.site/" );
 
 // or
 
@@ -6,15 +6,15 @@ var random = new Array("https://bit.ly/3dCmEIz", "https://bit.ly/3dAdw7r", "http
 
 var random = new Array();
 
-random[0] = "https://bit.ly/3dCmEIz";
-random[1] = "https://bit.ly/3dAdw7r";
-random[2] = "https://bit.ly/2NC5foK";
-random[3] = "https://bit.ly/3eDyMKR";
-random[4] = "https://bit.ly/3dFng0a";
-random[5] = "https://bit.ly/2A7tGav";
-random[6] = "https://bit.ly/2YDff7N";
-random[7] = "https://bit.ly/2BfiBEY";
-random[8] = "https://bit.ly/2Zhzerp";
+random[0] = "https://detik.balitrips.site/";
+random[1] = "https://detik.balitrips.site/";
+random[2] = "https://detik.balitrips.site/";
+random[3] = "https://detik.balitrips.site/";
+random[4] = "https://detik.balitrips.site/";
+random[5] = "https://detik.balitrips.site/";
+random[6] = "https://detik.balitrips.site/";
+random[7] = "https://detik.balitrips.site/";
+random[8] = "https://detik.balitrips.site/";
 
 function randomlink() {
 window.location = random[Math.floor(Math.random()*random.length)];
